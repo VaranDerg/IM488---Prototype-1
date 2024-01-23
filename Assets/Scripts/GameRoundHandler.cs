ae79b7813cb2aee272dbb296b1445c25bc71f162
rswansonbradley <raswanson@mail.bradley.edu>
2024-01-22 18:30:25 -0600

    GameRounds

---
+++ Assets/Scripts/GameRoundHandler.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameRoundHandler : MonoBehaviour
+{
+    [SerializeField] private float _roundTimerLength;
+    private float _currentRoundTime;
+    private float _p1Score;
+    private float _p2Score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void RoundStart()
+    {
+        StartCoroutine(RoundTimerCountDown());
+    }
+
+    public void RoundEnd()
+    {
+
+    }
+
+    private IEnumerator RoundTimerCountDown()
+    {
+        _currentRoundTime = _roundTimerLength;
+        while(true)
+        {
+            _currentRoundTime -= Time.deltaTime;
+        }
+    }
+}
