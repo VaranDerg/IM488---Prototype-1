b34c2552d6322fefb73ee7de6bd7d625a24d4856
Nabil Tagba <nabiltagba@gmail.com>
2024-01-27 01:01:19 -0600

    begining of shroomland level

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+
+    [SerializeField] float speed;
+
+    bool getDefensive;
+
+
+    private void Start()
+    {
+        getDefensive = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        print(getDefensive);
+        if (getDefensive)
+        {
+            transform.Rotate(0, 1 * speed, 0);
+
+        }
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" || other.gameObject.tag == "PhyInt")
+        {
+            //start spinning
+            getDefensive = true;
+            //apply damage
+        }
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+         if (other.gameObject.tag == "Player" || other.gameObject.tag == "PhyInt")
+        {
+            //stop spining
+            getDefensive = false;
+
+            
+        }
+    }
+
+}
