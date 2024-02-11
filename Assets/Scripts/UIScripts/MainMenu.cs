311463ab3859440eff7209a90fefdb4d0e0d45be
Rudy <rwolfer8@gmail.com>
2024-02-11 13:37:08 -0600

    Handbook Done

--- Assets/Scripts/UIScripts/MainMenu.cs
+++ Assets/Scripts/UIScripts/MainMenu.cs
@@ -8,12 +8,33 @@ using UnityEngine;
 /// </summary>
 public class MainMenu : BaseMenuController
 {
+    private static bool _seenFirstTimeText;
+    [SerializeField] private GameObject _firstTimeText;
+
+    private void Start()
+    {
+        if (!_seenFirstTimeText)
+        {
+            _seenFirstTimeText = true;
+            _firstTimeText.SetActive(true);
+        }
+        else
+        {
+            _firstTimeText.SetActive(false);
+        }
+    }
+
     /// <summary>
     /// Closes the game.
     /// </summary>
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void DisableHandbookText()
+    {
+        _firstTimeText.SetActive(false);
     }
 
     /// <summary>
